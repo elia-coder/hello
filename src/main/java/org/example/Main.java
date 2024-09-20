e33bf7b8ff149a3d719ab4ecc325fb29fd56855f
orikaskas <orikaskas1234@gmail.com>
2024-09-20 12:49:12 +0300

    innit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,79 @@
+package org.example;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+//TARGIL 1
+        System.out.println("hello world");
+        Scanner sc = new Scanner(System.in);
+
+//TARGIL 2
+
+//        System.out.println("write num 1");
+//        int num1 = sc.nextInt();
+//
+//        System.out.println("write num 2");
+//        int num2 = sc.nextInt();
+//
+//        int sum = num1-num2;
+//        System.out.println("+ " + (num1+num2));
+//        System.out.println("/ " + (double)num1/num2);
+//        System.out.println("* " + num1*num2);
+//        System.out.println("- " + sum);
+
+        //TARGIL 3
+//        System.out.println("write num 3");
+//        int num3 = sc.nextInt();
+//         if(num3%2==0)
+//             System.out.println("zogi");
+//         else   System.out.println("not zogi");
+
+        //TARGIL 4
+//        System.out.println("write the first num");
+//        int num4 = sc.nextInt();
+//        System.out.println("write the second num");
+//        int num5 = sc.nextInt();
+//        System.out.println("write the third num");
+//        int num6 = sc.nextInt();
+//
+//        double avg = (double)(num4+num5+num6)/3;
+//        System.out.println("their avg is " + avg);
+
+        //TARGIL 5
+//        System.out.println("your grade");
+//        int grade = sc.nextInt();
+//
+//        if (grade>90){
+//            System.out.println("great");
+//        } else if (grade<90&&grade>80) {
+//            System.out.println("nice ");
+//        } else if (grade<80&&grade>70) {
+//            System.out.println("ok");
+//        }
+//        else{System.out.println("fail");
+//    }
+
+//        TARGIL 6
+
+//        for (int i =0; i<11;i++)
+//        {
+//            System.out.println(i);
+//        }
+
+
+        //TARGIL 7
+//        int num0=10;
+//        while (num0>=0){
+//            System.out.println(num0);
+//            num0--;
+//        }
+//
+        //TARGIL10
+        int[]arr=new int[5];
+        for (int i =0;i<arr.length;i++){
+            System.out.println(arr[i]);
+        }
+
+    }
+    }
+
